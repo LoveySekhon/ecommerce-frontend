df69ca13f2e1097f86ade9ffae9ffd6823f88f16
Lovepreet Singh <lovepreetsingh7814893@gmail.com>
2026-02-18 13:48:51 +0530

    Updated API base URL to production backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ecommerce-backend-api-1mbm.onrender.com/api",
+  baseURL: "https://ecommerce-backend-api-1mbm.onrender.com/api"
 });
+
 
 export default api;
